b3a88cfa1b6871b2ba3b305b5c4527341076f448
Marcos Acosta <macosta1625@gmail.com>
2024-08-31 18:27:34 -0400

    Redo everything, connect to mongodb, use optimistic rendering because life is short

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,51 @@
+import bodyParser from "body-parser";
 import express from "express";
-import { GOAL_LOGS, ITEMS } from "./fake_data";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import * as dotenv from "dotenv";
 
 const app = express();
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export async function getItems() {}
 
 app.get("/", (req, res) => {
   res.send("Yes, it's up.");
 });
 
-app.get("/goal_logs", (req, res) => {
-  res.send(GOAL_LOGS);
+app.get("/items", async (req, res) => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const items = client.db("q").collection("items");
+    res.send(await items.find({}).toArray());
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 });
 
-app.get("/items", (req, res) => {
-  res.send(ITEMS);
+app.post("/items", async (req, res) => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const items = client.db("q").collection("items");
+    res.send(await items.insertOne(req.body));
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 });
 
 app.listen(3000, () => {
